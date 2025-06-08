b1ac3e0f166ccc45afea5b6eaf0efa75324c2044
David <ashuaib1991@gmail.com>
2025-06-08 23:19:34 +0100

    server server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-// server.js
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
